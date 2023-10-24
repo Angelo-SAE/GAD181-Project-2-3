bf831c1cfe3d4097398a35dc047d6d846a997b5b
Angelo <a.allaham@student.saedubai.com>
2023-10-24 16:38:41 +0400

    Fixed Error with Player Script, Created Enemy Scri

---
+++ Assets/_GAD181 Project 2-3/Scripts/Enemy.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float movementSpeed = 1.0f; 
+    public float AttackRange = 5.0f; 
+    public Transform player;
+    public int damage;
+
+    private void Update()
+    {
+        if (IsPlayerInRange())
+        {
+            MoveTowardsPlayer();
+        }
+    }
+
+    private bool IsPlayerInRange()
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+        return distanceToPlayer <= AttackRange;
+    }
+
+    private void MoveTowardsPlayer()
+    {
+        Vector3 direction = (player.position - transform.position).normalized;
+        transform.Translate(direction * movementSpeed * Time.deltaTime);
+    }
+}
